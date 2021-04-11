ab5743209e28aa76e997645a8be982c77b9b28ae
Miriam Ahlberg <miriam.ahlberg@gmail.com>
2021-04-11 18:02:14 +0200

    Styled components -> stitches

--- src/components/panes/PaymentPane/Bank/ResultPane.style.tsx
+++ src/components/panes/PaymentPane/Bank/ResultPane.style.tsx
@@ -1,64 +1,66 @@
-import styled from 'styled-components'
+import { styled } from '../../../../styles/stitches.config'
 
-import { gray18, grey14 } from '../../../../config/colors'
+export const ResultWrapper = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+})
 
-export const ResultWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
+export const VippsLogo = styled('img', {
+  width: '120px',
+  margin: '20px',
+})
 
-export const VippsLogo = styled.img`
-  width: 120px;
-  margin: 20px;
-`
+export const Heart = styled('img', {
+  width: '50px',
+  height: '50px',
+})
 
-export const Heart = styled.img`
-  width: 50px;
-  height: 50px;
-`
+export const BlackContainer = styled('div', {
+  backgroundColor: 'black',
+  width: '100%',
+})
 
-export const BlackContainer = styled.div`
-  background-color: black;
-  width: 100%;
-`
+export const BoldWhiteText = styled('p', {
+  color: 'white',
+  fontWeight: 'bold',
+})
 
-export const BoldWhiteText = styled.p`
-  color: white;
-  display: inline;
-  font-weight: bold;
-`
+export const WhiteText = styled('p', {
+  color: 'white',
+  display: 'inline',
+  marginLeft: '5px',
+})
 
-export const WhiteText = styled.p`
-  color: white;
-  display: inline;
-  margin-left: 5px;
-`
+export const TextWrapper = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+})
 
-export const TextWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-`
+export const InfoText = styled('div', {
+  textAlign: 'center',
+  fontSize: '13px',
+  padding: '10px',
+})
 
-export const InfoText = styled.div`
-  text-align: center;
-  font-size: 13px;
-  padding: 10px;
-`
+export const PaymentDetailsWrapper = styled('div', {
+  border: '1px solid $grey18',
+  borderRadius: '5px',
+  marginBottom: '10px',
+  marginTop: '10px',
+})
 
-export const PaymentDetailsWrapper = styled.div`
-  border: 1px solid ${gray18};
-  border-radius: 5px;
-  margin-bottom: 10px;
-  margin-top: 10px;
-`
-export const DetailsRow = styled.div`
-  border-bottom: 1px solid ${grey14};
-  display: flex;
-  justify-content: space-between;
-  padding: 10px;
+export const DetailsRow = styled('div', {
+  borderBottom: '1px solid $grey14',
+  display: 'flex',
+  justifyContent: 'space-between',
+  padding: '$s100',
 
-  :last-child {
-    border-bottom: none;
-  }
-`
+  ':last-child': {
+    borderBottom: 'none',
+  },
+})
+
+export const BoldText = styled('p', {
+  fontWeight: 'bold',
+})
--- src/config/colors.ts
+++ src/config/colors.ts
@@ -1,4 +1,3 @@
 export const orange15 = '#FFBB55'
 
 export const gray18 = '#B9B9B9'
-export const grey14 = '#DBDBDB'
