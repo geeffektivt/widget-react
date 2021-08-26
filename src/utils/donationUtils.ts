7da0ab0af601af9ef4777780035c085c45152436
Miriam Ahlberg <miriam.ahlberg@gmail.com>
2021-08-26 17:13:46 +0200

    Prioritize non-zero unlocked causes over zero ones

--- src/utils/donationUtils.ts
+++ src/utils/donationUtils.ts
@@ -83,13 +83,13 @@ export function updateValues(
 
     const entry = entries[roundRobinIndex]
 
-    // If a cause was set to 0, then it should be kept at 0 unless the remaining causes also are 0 or locked
+    // If a cause was set to 0, then it should be kept at 0 unless all the remaining causes also are 0 or locked
     // ie only update a 0-cause when there is no other option
     const shouldStickToZero =
       entry.share === 0 &&
       entries
         .filter((_, i) => i !== updatedIndex)
-        .filter((e) => e.share === 0 || e.isLocked).length === 1
+        .filter((e) => e.share > 0 && !e.isLocked).length > 0
 
     if (entry.isLocked || shouldStickToZero) {
       // don't update this slider
