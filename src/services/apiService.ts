8ee71355a6e81fbf2aae1267319f4b9b92dfd9f3
Miriam Ahlberg <miriam.ahlberg@gmail.com>
2021-12-24 09:38:11 +0100

    More error logging

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -17,12 +17,14 @@ export async function apiRequest<T = unknown>(
   const responseData = await attempt(parseResponseData<T>(response))
 
   if (hasFailed(responseData)) {
+    console.error('Failed to parse response', responseData)
     throw AppError.fromError('Failed to parse response', responseData.error, {
       extraData: responseData,
     })
   }
 
   if (response.ok !== true) {
+    console.error('Response not ok', responseData)
     throw AppError.fromResponse(response, { extraData: responseData })
   }
 
